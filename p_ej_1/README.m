4531a1bac040151cb048da9646bbb3a25f096cd6
Ricardo Mena C <rgacort12@gmail.com>
2020-11-29 12:31:08 -0600

    Organization projects
    Signed-off-by: Ricardo Mena C <rgacort12@gmail.com>

--- p_ej_1/README.m
+++
@@ -1,5 +0,0 @@
-#Compile and Test
-
-$iverilog AND.v OR.v XOR.v TestB_tb.v -o testb.out && vvp testb.out && gtkwave testbench.vc
-
-
---
+++ problema_1/README.m
@@ -0,0 +1,15 @@
+#Compile and Test
+
+Se dispone de un Makefile para compilar todos los archivos y crear el banco de pruebas necesario.
+
+Ejercio 1
+
+* AND.v módulo de una AND de 4 entradas
+* OR.v módulo de una OR de 4 entradas
+* XOR.v módulo de una XOR de 4 entradas
+* MUX4x1.v módulo de multiplexor de entradas y salida de 4bits
+* fad_cell.v (full adder cell), módulo de una celda típica para constuir un sumador completo de 4 bits
+* full_adder.v módulo de sumador completo de 4 bits
+* test_b_ejercicio_uno.v "banco" de pruebas realizada a los módulos creados para ele ejercicio 1
+* test_b_ejercicio_dos.v "banco" de pruebas realizada a los módulos creados para ele ejercicio 2
+
